03d8b4c893d5275ee097a94792c218bc50bda5dc
cuihongpeng <cuihongpeng@tsinghua-wx.org>
2018-08-16 09:43:35 +0800

    fatfs: fix error

--- source/diskio.c
+++ source/diskio.c
@@ -7,7 +7,7 @@
 /* storage control modules to the FatFs module with a defined API.       */
 /*-----------------------------------------------------------------------*/
 
-#include "diskio.h"		/* FatFs lower layer API */
+#include "diskio.h"     /* FatFs lower layer API */
 
 #include <stdio.h>
 #include <string.h>
@@ -23,10 +23,10 @@ int f_fatfs = -1;
 /*-----------------------------------------------------------------------*/
 
 DSTATUS disk_status (
-	BYTE pdrv		/* Physical drive nmuber to identify the drive */
+    BYTE pdrv       /* Physical drive nmuber to identify the drive */
 )
 {
-	return 0;
+    return 0;
 }
 
 /*-----------------------------------------------------------------------*/
@@ -34,16 +34,16 @@ DSTATUS disk_status (
 /*-----------------------------------------------------------------------*/
 
 DSTATUS disk_initialize (
-	BYTE pdrv				/* Physical drive nmuber to identify the drive */
+    BYTE pdrv               /* Physical drive nmuber to identify the drive */
 )
 {
   DISK_DEBUG("  [%s fs] disk_initialize\n", USE_RAM ? "RAM" : "DISK");
   
 #if (USE_RAM == 0)
-	if (f_fatfs == -1)
-		f_fatfs = open(DISK_NAME, O_SYNC | O_RDWR, S_IRUSR | S_IWUSR);
+    if (f_fatfs == -1)
+        f_fatfs = open(DISK_NAME, O_SYNC | O_RDWR, S_IRUSR | S_IWUSR);
 
-	lseek(f_fatfs, 0, SEEK_SET);
+    lseek(f_fatfs, 0, SEEK_SET);
 #endif
 
   return RES_OK;
@@ -54,22 +54,22 @@ DSTATUS disk_initialize (
 /*-----------------------------------------------------------------------*/
 
 DRESULT disk_read (
-	BYTE pdrv,		/* Physical drive nmuber to identify the drive */
-	BYTE *buff,		/* Data buffer to store read data */
-	DWORD sector,	/* Start sector in LBA */
-	UINT count		/* Number of sectors to read */
+    BYTE pdrv,      /* Physical drive nmuber to identify the drive */
+    BYTE *buff,     /* Data buffer to store read data */
+    DWORD sector,   /* Start sector in LBA */
+    UINT count      /* Number of sectors to read */
 )
 {
-	DISK_DEBUG("  [%s fs] disk_read %ld %d\n", USE_RAM ? "RAM" : "DISK", sector, count);
-#if USE_RAM	
-	void *p_disk = (void *)(disk + sector * SEC_SIZE);
-	memcpy(buff, p_disk, count * SEC_SIZE);
+    DISK_DEBUG("  [%s fs] disk_read %ld %d\n", USE_RAM ? "RAM" : "DISK", sector, count);
+#if USE_RAM 
+    void *p_disk = (void *)(disk + sector * SEC_SIZE);
+    memcpy(buff, p_disk, count * SEC_SIZE);
 #else
-	lseek(f_fatfs, sector * SEC_SIZE, SEEK_SET);
-	read(f_fatfs, buff, count * SEC_SIZE);
+    lseek(f_fatfs, sector * SEC_SIZE, SEEK_SET);
+    read(f_fatfs, buff, count * SEC_SIZE);
 #endif
 
-	return RES_OK;
+    return RES_OK;
 }
 
 /*-----------------------------------------------------------------------*/
@@ -77,22 +77,22 @@ DRESULT disk_read (
 /*-----------------------------------------------------------------------*/
 
 DRESULT disk_write (
-	BYTE pdrv,			/* Physical drive nmuber to identify the drive */
-	const BYTE *buff,	/* Data to be written */
-	DWORD sector,		/* Start sector in LBA */
-	UINT count			/* Number of sectors to write */
+    BYTE pdrv,          /* Physical drive nmuber to identify the drive */
+    const BYTE *buff,   /* Data to be written */
+    DWORD sector,       /* Start sector in LBA */
+    UINT count          /* Number of sectors to write */
 )
 {
-	DISK_DEBUG("  [%s fs] disk_write %ld %d\n", USE_RAM ? "RAM" : "DISK", sector, count);	
-#if USE_RAM	
-	void *p_disk = (void *)(disk + sector * SEC_SIZE);
-	memcpy(p_disk, buff, count * SEC_SIZE);
+    DISK_DEBUG("  [%s fs] disk_write %ld %d\n", USE_RAM ? "RAM" : "DISK", sector, count);   
+#if USE_RAM 
+    void *p_disk = (void *)(disk + sector * SEC_SIZE);
+    memcpy(p_disk, buff, count * SEC_SIZE);
 #else
-	lseek(f_fatfs, sector * SEC_SIZE, SEEK_SET);
-	write(f_fatfs, buff, count * SEC_SIZE);
+    lseek(f_fatfs, sector * SEC_SIZE, SEEK_SET);
+    write(f_fatfs, buff, count * SEC_SIZE);
 #endif
 
-	return RES_OK;
+    return RES_OK;
 }
 
 /*-----------------------------------------------------------------------*/
@@ -100,33 +100,33 @@ DRESULT disk_write (
 /*-----------------------------------------------------------------------*/
 
 DRESULT disk_ioctl (
-	BYTE pdrv,		/* Physical drive nmuber (0..) */
-	BYTE cmd,		/* Control code */
-	void *buff		/* Buffer to send/receive control data */
+    BYTE pdrv,      /* Physical drive nmuber (0..) */
+    BYTE cmd,       /* Control code */
+    void *buff      /* Buffer to send/receive control data */
 )
 {
-	DISK_DEBUG("  [%s fs] disk_ioctl %d %d\n", USE_RAM ? "RAM" : "DISK", pdrv, cmd);	
+    DISK_DEBUG("  [%s fs] disk_ioctl %d %d\n", USE_RAM ? "RAM" : "DISK", pdrv, cmd);    
 
-	if (cmd == CTRL_SYNC) 
-	  return RES_OK;	  
+    if (cmd == CTRL_SYNC) 
+      return RES_OK;      
 
-	if (cmd == GET_SECTOR_COUNT) 
-	{
-	  *((DWORD*)buff) = SEC_COUNT;
-	  return RES_OK;	  
-	}
+    if (cmd == GET_SECTOR_COUNT) 
+    {
+      *((DWORD*)buff) = SEC_COUNT;
+      return RES_OK;      
+    }
 
-	if (cmd == GET_BLOCK_SIZE) 
-	{
-	  *((WORD*)buff) = (WORD)(SEC_COUNT * SEC_SIZE);
-	  return RES_OK;
-	}
+    if (cmd == GET_BLOCK_SIZE) 
+    {
+        *((WORD *)buff) = 2;
+        return RES_OK;
+    }
 
-	return RES_PARERR;
+    return RES_PARERR;
 }
 
 DWORD get_fattime (void)
 {
 
-	return 0;
+    return 0;
 }
